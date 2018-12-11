64232d6c89b780a8d1d37ca07a2e7d7dd5fb53a4
Alex <ImOutOfAlex@her.farm>
2018-12-10 20:47:52 -0800

    Fix linter shit.

--- bot1.py
+++ bot1.py
@@ -9,11 +9,11 @@ parser.add_argument("token", help="BOT",
 args = parser.parse_args()
 
 
-
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
 There are a number of utility commands being showcased here.'''
 bot = commands.Bot(command_prefix='8=D', description=description)
+
 
 @bot.event
 async def on_ready():
@@ -22,13 +22,15 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+
 @bot.command()
-async def add(left : int, right : int):
+async def add(left: int, right: int):
     """Adds two numbers together."""
     await bot.say(left + right)
 
+
 @bot.command()
-async def roll(dice : str):
+async def roll(dice: str):
     """Rolls a dice in NdN format."""
     try:
         rolls, limit = map(int, dice.split('d'))
@@ -39,21 +41,25 @@ async def roll(dice : str):
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await bot.say(result)
 
+
 @bot.command(description='For when you wanna settle the score some other way')
-async def choose(*choices : str):
+async def choose(*choices: str):
     """Chooses between multiple choices."""
     await bot.say(random.choice(choices))
 
+
 @bot.command()
-async def repeat(times : int, content='repeating...'):
+async def repeat(times: int, content='repeating...'):
     """Repeats a message multiple times."""
     for i in range(times):
         await bot.say(content)
 
+
 @bot.command()
-async def joined(member : discord.Member):
+async def joined(member: discord.Member):
     """Says when a member joined."""
     await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+
 
 @bot.group(pass_context=True)
 async def cool(ctx):
@@ -63,8 +69,11 @@ async def cool(ctx):
     if ctx.invoked_subcommand is None:
         await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
 
+
 @cool.command(name='bot')
 async def _bot():
     """Is the bot cool?"""
     await bot.say('Yes, the bot is cool.')
+
+
 bot.run(args.token)
